53516768389f0468be42848153b6ed6771a9e524
Lambda026 <3222707117@qq.com>
2025-05-04 20:01:28 +0800

    实验一21221134

---
+++ os/src/console.rs
@@ -0,0 +1,58 @@
+#![no_std]
+
+use core::fmt::{self, Write};
+use core::arch::asm;
+
+const SYSCALL_EXIT: usize = 93;
+const SYSCALL_WRITE: usize = 64;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!(
+            "ecall",
+            in("x10") args[0],
+            in("x11") args[1],
+            in("x12") args[2],
+            in("x17") id,
+            lateout("x10") ret,
+        );
+    }
+    ret
+}
+
+pub fn sys_exit(code: i32) -> ! {
+    syscall(SYSCALL_EXIT, [code as usize, 0, 0]);
+    loop {}
+}
+
+pub fn sys_write(fd: usize, buf: &[u8]) -> isize {
+    syscall(SYSCALL_WRITE, [fd, buf.as_ptr() as usize, buf.len()])
+}
+
+struct Stdout;
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn _print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::console::_print(format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($fmt:expr) => ($crate::print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::print!(concat!($fmt, "\n"), $($arg)*));
+}
+
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,9 +1,20 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
+mod console;
+
+use console::sys_exit;
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    sys_exit(-1)
 }
+
+#[no_mangle]
+extern "C" fn _start() -> ! {
+    println!("Hello, world!");
+    sys_exit(0)
+}
+
